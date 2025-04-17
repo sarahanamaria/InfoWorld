ea32a659c7f382f49482df1f074c5ecd437fc553
sarahanamaria <sarahanamaria@yahoo.com>
2025-04-18 02:40:38 +0300

    Create empty components

---
+++ src/components/add-client-form/add-client-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-client-form',
+  imports: [],
+  templateUrl: './add-client-form.component.html',
+  styleUrl: './add-client-form.component.scss'
+})
+export class AddClientFormComponent {
+
+}
